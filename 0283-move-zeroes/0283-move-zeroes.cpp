81ebcd061f65279cd101cb14456fcdccaa0afb04
Biplab Bhowmik <bbhowmik214@gmail.com>
2026-09-07 11:26:17 +0530

    Time: 0 ms (100%), Space: 24 MB (19.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        if(nums.empty()) return;
         int slow = 0;
         for(int i=0; i<nums.size(); ++i){
             if(nums[i]!=0){
-                swap(nums[slow], nums[i]);
+                if(slow!=i){
+                    swap(nums[slow], nums[i]);
+                }
                 ++slow;
             }
 
